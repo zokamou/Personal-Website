056f8b158532d3776f0014add58f28c5330bb659
zfeller <zfeller@ucsc.edu>
2024-09-03 13:33:23 -0700

    some changes for senior year

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import profilePic from "./assets/profile_pic.jpg";
 import styles from "./page.module.css";
+import React from 'react';
+import {Button} from '@mui/material';
 
 export default function Home() {
   return (
@@ -9,13 +11,30 @@ export default function Home() {
       <div className={styles.main}>
         <div className={styles.pfpLayout}>
           <Image className={styles.pfp} src={profilePic} alt="pfp" />
-          <p className={styles.description}>Hello! My name is Zoe Feller and I am a junior at
-          UC Santa Cruz. I am studying Computer Science Game Design with a minor in Computer Science.
-          I am extremely passionate about game design and full-stack web development. Here are a few of the projects
-          I have worked on in the past year, enjoy!
+          <p className={styles.description}>
+            Hello! <br></br> <br></br> 
+            My name is Zoe Feller and I am a senior at
+            UC Santa Cruz. I am studying Computer Science Game Design with a minor in Computer Science.
+            I am extremely passionate about game design and full-stack web development. Here are a few of the projects
+            I have worked on in the past year. <br></br> <br></br> 
+            Enjoy!
           </p>
         </div>
         <div className={styles.grid}>
+        <a
+            href="https://www.niletrade.xyz/"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              NileTrade <span>-&gt;</span>
+            </h2>
+            <p>Junior capstone project. Developed a full-stack web app 
+              with five other students mocking Amazon's website. Feel free to explore, make an account,
+              or even check out as a guest! To use the test Stripe, the card number is 4242 4242 4242 4242
+              with any expiration and CVV. </p>
+          </a>
           <a
             href="https://zokamou.github.io/Wind-Up/"
             className={styles.card}
